521e11ea2947e98605f95ee611c675b9ff4ad15a
andrerocker <andre.souza@gmail.com>
2011-12-06 10:05:23 -0200

    melhorando script server.sh

--- c/sockets/server.sh
+++ c/sockets/server.sh
@@ -1,2 +1,14 @@
 #/bin/bash
-nc -l 1337 < client.c
+system=$(uname -s);
+port=1337
+
+if [[ $system == "Linux" ]]
+then
+  # Possivel GNU Netcat \o/
+  echo "starting with gnu params"
+  nc -v -l -p $port -q 1 < client.c
+else
+  # Possivel BSD Netcat :(
+  echo "starting with bsd params"
+  nc -l $port < client.c
+fi
